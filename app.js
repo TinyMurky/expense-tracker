db6ec6d3a9a6c05b711d31c45b1e555c32ee730b
TinyMurky <murky0830@gmail.com>
2023-06-10 13:29:12 +0800

    init basic middleware

--- app.js
+++ app.js
@@ -16,3 +16,6 @@ app.engine('handlebars', exphbs.engine({
   helpers: require('./plugins/handlebars-helpers')
 }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
