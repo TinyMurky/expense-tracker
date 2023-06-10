96f42fa01ffcc8e70616c67e4c2e97bc1a02989a
TinyMurky <murky0830@gmail.com>
2023-06-10 18:25:38 +0800

    Add js and css display in index.hbs

--- routes/modules/entries.js
+++ routes/modules/entries.js
@@ -2,5 +2,5 @@ import express from 'express'
 export const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index', {})
+  res.render('index', { stylesheet: 'index.css', script: 'index.js' })
 })
