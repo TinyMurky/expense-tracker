8a000d68fb85af8dfaddb1f0315c37dc50120414
TinyMurky <murky0830@gmail.com>
2023-06-11 19:45:59 +0800

    Create category model for recording entries category

--- models/category.js
+++ models/category.js
@@ -1,8 +1,30 @@
 import mongoose from 'mongoose'
+import Counter from './counter.js'
 const CategorySchema = new mongoose.Schema({
   _id: {
     type: Number,
     require: [true, 'Category need an _id']
+  },
+  name: {
+    type: String,
+    require: [true, 'Category document need a name']
+  }
+})
+CategorySchema.pre('save', async function (next) {
+  try {
+    const doc = this // 代表現在新增的document
+    const counter = await Counter.findByIdAndUpdate(
+      { _id: 'categoryId' }, // 找到Couter中紀錄的model_id
+      { $inc: { sequence: 1 } }, // sequence + 1
+      { new: true, upsert: true }
+      // new: true => findByIdAndUpdate會回傳未更新的document, true可以回傳更新過的
+      // upsert: true 如果沒有就建立一個新的counter document
+    ).exec()
+
+    doc._id = counter.sequence // 新建的category _id = sequence
+    next() // 儲存
+  } catch (error) {
+    next(error)
   }
 })
 
