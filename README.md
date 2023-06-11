a9d620b3dd61539aff08e8d37133151376ab7a0d
TinyMurky <murky0830@gmail.com>
2023-06-11 15:22:29 +0800

    Created /entries/new html page  for adding new entry

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 |:--|:--|:--|
 |呈現全部|GET|`/entries`|
 |呈現新增的畫面|GET|`/entries/new`|
-|新增一筆|POST|`/entries/new`|
+|新增一筆|POST|`/entries`|
 |呈現修改畫面|GET|`/entries/:id/edit`|
 |修改一筆|PUT|`/entries/:id`|
 |刪除一筆|DELETE|`/entries/:id`|
---
+++ public/javascripts/new.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form')
+const submitBtn = document.querySelector('#submit-btn')
+form.addEventListener('submit', (event) => {
+  if (!form.checkValidity()) {
+    event.preventDefault() // 不要submit
+    event.stopPropagation() // event不要bubble
+  }
+})
+submitBtn.addEventListener('click', (event) => {
+  form.classList.add('was-validated')
+})
