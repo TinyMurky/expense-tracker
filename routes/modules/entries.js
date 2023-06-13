2ce0701e9e8f2adb7969823e0f942e5b3c25eb86
TinyMurky <murky0830@gmail.com>
2023-06-13 16:20:51 +0800

    [Feat]Error message now can display on new and index page

--- routes/modules/entries.js
+++ routes/modules/entries.js
@@ -8,7 +8,10 @@ const categories = await getCategory()
 
 // 新增支出的頁面
 router.get('/new', (req, res) => {
-  res.render('new', { stylesheet: 'new.css', script: 'new.js', categories })
+  // 在post的時候出現錯誤從此取出
+  const newErrors = req.flash('new_errors')[0]
+
+  res.render('new', { stylesheet: 'new.css', script: 'new.js', categories, newErrors })
 })
 // 創建新的支出
 router.post('/', async (req, res) => {
@@ -27,12 +30,17 @@ router.post('/', async (req, res) => {
     res.redirect('/entries')
   } catch (error) {
     console.error(error)
+    // 如果出現任何錯誤就放入flash後導回/new_頁面
+    req.flash('new_errors', error.errors)
+    res.redirect('/entries/new')
   }
 })
 
 // 主要頁面 categoryID非必要，若無則全部顯示
 router.get('/:categoryID?', async (req, res) => {
   try {
+    // 如果主畫面 與 delete出現error都會放入indexError並從此取出
+    const indexErrors = req.flash('index_page_error')
     const categoryID = req.params.categoryID
     const query = { userID: 0 }
     if (categoryID) { // 如果沒有提供categoryID就不用該條件搜尋，以呈現所有records
@@ -44,9 +52,11 @@ router.get('/:categoryID?', async (req, res) => {
       entry.date = entry.date.toLocaleDateString('zh-TW')
       totalSpend += entry.amount
     }
-    res.render('index', { stylesheet: 'index.css', script: 'index.js', entries, totalSpend, categories })
+    res.render('index', { stylesheet: 'index.css', script: 'index.js', entries, totalSpend, categories, indexErrors })
   } catch (error) {
     console.error(error)
+    req.flash('index_page_error', error.errors)
+    res.redirect('/entries')
   }
 })
 
@@ -58,5 +68,7 @@ router.delete('/:id', async (req, res) => {
     res.redirect('/entries')
   } catch (error) {
     console.error(error)
+    req.flash('index_page_error', error.message)
+    res.redirect('/entries')
   }
 })
