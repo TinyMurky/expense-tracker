4347923395e47c194b644b5579a564c5cf1c2139
TinyMurky <murky0830@gmail.com>
2023-06-13 15:08:16 +0800

    [Feat]new entries now can only input date before or equal today

--- routes/modules/entries.js
+++ routes/modules/entries.js
@@ -12,41 +12,51 @@ router.get('/new', (req, res) => {
 })
 // 創建新的支出
 router.post('/', async (req, res) => {
-  // { name: 'a', date: '2023-06-12', category: '1', number: '1' }
-  let { name, date, categoryID, amount } = req.body
-  date = new Date(date)
-  amount = parseInt(amount, 10)
-  const entry = new Record({
-    name,
-    date,
-    amount,
-    userID: 0,
-    categoryID
-  })
-  await entry.save()
-  res.redirect('/entries')
+  try {
+    let { name, date, categoryID, amount } = req.body
+    date = new Date(date)
+    amount = parseInt(amount, 10)
+    const entry = new Record({
+      name,
+      date,
+      amount,
+      userID: 0,
+      categoryID
+    })
+    await entry.save()
+    res.redirect('/entries')
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 // 主要頁面 categoryID非必要，若無則全部顯示
 router.get('/:categoryID?', async (req, res) => {
-  const categoryID = req.params.categoryID
-  const query = { userID: 0 }
-  if (categoryID) { // 如果沒有提供categoryID就不用該條件搜尋，以呈現所有records
-    query.categoryID = categoryID
+  try {
+    const categoryID = req.params.categoryID
+    const query = { userID: 0 }
+    if (categoryID) { // 如果沒有提供categoryID就不用該條件搜尋，以呈現所有records
+      query.categoryID = categoryID
+    }
+    const entries = await Record.find(query).sort({ date: -1, _id: -1 }).lean()
+    let totalSpend = 0
+    for (const entry of entries) {
+      entry.date = entry.date.toLocaleDateString('zh-TW')
+      totalSpend += entry.amount
+    }
+    res.render('index', { stylesheet: 'index.css', script: 'index.js', entries, totalSpend, categories })
+  } catch (error) {
+    console.error(error)
   }
-  const entries = await Record.find(query).sort({ date: -1, _id: -1 }).lean()
-  let totalSpend = 0
-  for (const entry of entries) {
-    entry.date = entry.date.toLocaleDateString('zh-TW')
-    totalSpend += entry.amount
-  }
-
-  res.render('index', { stylesheet: 'index.css', script: 'index.js', entries, totalSpend, categories })
 })
 
 // 刪除頁面
 router.delete('/:id', async (req, res) => {
-  const _id = parseInt(req.params.id, 10)
-  await Record.deleteOne({ _id, userID: 0 })
-  res.redirect('/entries')
+  try {
+    const _id = parseInt(req.params.id, 10)
+    await Record.deleteOne({ _id, userID: 0 })
+    res.redirect('/entries')
+  } catch (error) {
+    console.error(error)
+  }
 })
